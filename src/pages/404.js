fdb17ba25f9a2ac28a02be4a13b1ed2b61a03c7f
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-10-01 21:53:37 +1300

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class FourOhFour extends React.Component {
+  render() {
+    return <div>No page here brotato. Look elsewhere.</div>;
+  }
+}
+
+export default FourOhFour;
